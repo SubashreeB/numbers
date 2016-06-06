fce91b4407466d916960ee7b3ac126ef642f3b2f
SubashreeB <subabakth07@gmail.com>
2016-06-06 14:00:50 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a<0)
+   printf("Number is  Negative");
+  else if(a==0)
+      printf("Number is  equal to zero");
+  else
+       printf("Number is  positive");
+}
